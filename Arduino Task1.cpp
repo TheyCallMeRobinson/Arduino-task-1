2dcbdc4247dccf481b6fe11347ebb3396c4703e7
PaulRobinson834 <PaulRobinson834@users.noreply.github.com>
2019-09-24 20:14:40 +0300

    Update Arduino Task1.cpp

--- Arduino Task1.cpp
+++ Arduino Task1.cpp
@@ -1,25 +1,34 @@
+boolean button;
+
 void setup() {
-pinMode(13, OUTPUT);
-pinMode(12, OUTPUT);
-pinMode(8, OUTPUT);
+  
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(2, INPUT_PULLUP);
+  
 }
 
 void loop() {
- 
-  digitalWrite(13,HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-
-  digitalWrite(12, HIGH);
-  delay(1000);
-  digitalWrite(12,LOW);
-
-  digitalWrite(8, HIGH);
-  delay(1000);
-  digitalWrite(8,LOW);
-
-  digitalWrite(12, HIGH);
-  delay(1000);
-  digitalWrite(12, LOW);
+  
+    button = digitalRead(2);
+    
+    if (!button) {
+    digitalWrite(13,HIGH);
+    delay(1000);
+    digitalWrite(13, LOW);
+  
+    digitalWrite(12, HIGH);
+    delay(1000);
+    digitalWrite(12,LOW);
+  
+    digitalWrite(8, HIGH);
+    delay(1000);
+    digitalWrite(8,LOW);
+  
+    digitalWrite(12, HIGH);
+    delay(1000);
+    digitalWrite(12, LOW);
+  }
 
 }
